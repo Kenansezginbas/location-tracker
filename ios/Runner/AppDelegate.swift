58c96525f67c0cc3515b7d3b3eaff237a6efa0b6
KenanSezginbas <kenan.sezginbas@esfagrup.com>
2025-05-17 20:09:14 +0300

    fixed linter problems

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBnor6tdeWTvTH30TLyD_gy_FCox-7zY1M")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
